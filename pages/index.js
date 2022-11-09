75d7770804367e2528165a96c2b1f3549484bd85
Can Mihci <canmihci3@gmail.com>
2022-11-09 21:02:07 +0300

    successfully adding entries, working on edit and deletion

--- pages/index.js
+++ pages/index.js
@@ -1,19 +1,23 @@
-/* import Head from "next/head";
-import Image from "next/image";
-import styles from "../styles/Home.module.css"; */
+import styles from "../styles/Home.module.css";
 import { useForm } from "react-hook-form";
 import Axios from "axios";
-import { useState, useEffect } from "react";
-/* import useSWR, { useSWRConfig }  from "swr"; */
-
-/* const fetcher = (...args) => Axios.get(...args).then(res => res.data) */
+import { useState, useEffect, useRef } from "react";
 
 export default function Home() {
-  /*   const { mutate } = useSWRConfig()
-  const { data, error } = useSWR("/api/instructor", fetcher); */
-
   const [instructors, setInstructors] = useState(null);
   const [isLoading, setLoading] = useState(false);
+  const [highlight, setHighlight] = useState(null);
+  const [editMode, setEditMode] = useState(false);
+  const [editFirstName, setEditFirstName] = useState("");
+  const [editLastName, setEditLastName] = useState("");
+  const [highlightedInstructor, setHighlightedInstructor] = useState({
+    _id: "",
+    firstName: "",
+    lastName: "",
+  });
+
+  const firstNameRef = useRef("");
+  const lastNameRef = useRef("");
 
   useEffect(() => {
     setLoading(true);
@@ -24,19 +28,87 @@ export default function Home() {
     });
   }, []);
 
-  const { register, handleSubmit } = useForm();
-  const onSubmit = (submitData) => {
-    Axios.post("/api/instructor", submitData)
-      .then((response) => {
-        const {result} = response.data;
-        console.log(result)
-        if (result.acknowledged) {
-          console.log(submitData);
-          setInstructors([...instructors, {...submitData, "_id": result.insertedId}]);
-        }
-      })
-      .catch((error) => console.log(error));
-  };
+  function handleSubmit(e) {
+    e.preventDefault();
+    if (editFirstName && editLastName) {
+      const submitData = {
+        firstName: editFirstName,
+        lastName: editLastName,
+      };
+      Axios.post("/api/instructor", submitData)
+        .then((response) => {
+          const { result } = response.data;
+          console.log(result);
+          if (result.acknowledged) {
+            console.log(submitData);
+            setInstructors([
+              ...instructors,
+              { ...submitData, _id: result.insertedId },
+            ]);
+          }
+        })
+        .catch((error) => console.log(error));
+    } else {
+      console.log("can't submit empty");
+    }
+  }
+
+  function Instructor(props) {
+    const { instructor } = props;
+    return (
+      <div
+        className={`${instructor._id === highlight ? styles.highlight : ""} ${
+          styles["instructor-row"]
+        }`}
+        onClick={props.clickHandler}
+        data-id={instructor._id}
+        data-firstname={instructor.firstName}
+        data-lastname={instructor.lastName}
+      >
+        {instructor.firstName} {instructor.lastName}
+      </div>
+    );
+  }
+
+  function highlightInstructor(e) {
+    console.log("clicky");
+    const { id, firstname, lastname } = e.target.dataset;
+    console.log(e.target);
+    if (highlight === id) {
+      setHighlight(null);
+      setHighlightedInstructor({
+        _id: "",
+        firstName: "",
+        lastName: "",
+      });
+      firstNameRef.current.value = "";
+      lastNameRef.current.value = "";
+      setEditFirstName("");
+      setEditLastName("");
+      setEditMode(false);
+    } else {
+      setHighlight(id);
+      setHighlightedInstructor({
+        _id: id,
+        firstName: firstname,
+        lastName: lastname,
+      });
+      setEditMode(true);
+      firstNameRef.current.value = firstname;
+      lastNameRef.current.value = lastname;
+      setEditFirstName(firstNameRef.current.value);
+      setEditLastName(lastNameRef.current.value);
+    }
+  }
+
+  function handleFirstNameChange(e) {
+    setEditFirstName(firstNameRef.current.value);
+    console.log(editFirstName);
+  }
+  function handleLastNameChange(e) {
+    setEditLastName(lastNameRef.current.value);
+    console.log(editLastName);
+  }
 
   if (isLoading) return <div>Loading...</div>;
 
@@ -44,25 +116,38 @@ export default function Home() {
     <div>
       <h1>Project Started</h1>
       <div>
-        <form onSubmit={handleSubmit(onSubmit)}>
-          <label>
-            Adı:
-            <input {...register("firstName")} />
-          </label>
-          <label>
-            Soyadı:
-            <input {...register("lastName")} />
-          </label>
-          <input type="submit" value="Kaydet" />
+        <form className={styles.form} onSubmit={handleSubmit}>
+          <label>Adı:</label>
+          <input onChange={handleFirstNameChange} ref={firstNameRef} />
+          <label>Soyadı:</label>
+          <input onChange={handleLastNameChange} ref={lastNameRef} />
+          <div></div>
+          {editMode ? (
+            <div className={styles["edit-buttons"]}>
+              <button type="button"
+                disabled={
+                  highlightedInstructor.firstName === editFirstName &&
+                  highlightedInstructor.lastName === editLastName
+                }
+              >
+                Düzelt
+              </button>
+              <button type="button">Sil</button>
+            </div>
+          ) : (
+            <input type="submit" value="Yeni Ekle" />
+          )}
         </form>
       </div>
       <div>
         {instructors &&
           instructors.map((instructor) => {
             return (
-              <div key={instructor._id}>
-                {instructor.firstName} {instructor.lastName}{" "}
-              </div>
+              <Instructor
+                instructor={instructor}
+                key={instructor._id}
+                clickHandler={highlightInstructor}
+              />
             );
           })}
       </div>
